22c1785d01fbb6f9417c15d57983a6acc15abd90
manu-yaff <manuelyg2097@gmail.com>
2022-06-09 04:29:44 -0500

    remove test file

--- src/index.tsx
+++ src/index.tsx
@@ -34,11 +34,11 @@ const client = new ApolloClient({
 });
 
 root.render(
-  <React.StrictMode>
-		<ApolloProvider client={client}>
-			<App />
-		</ApolloProvider>
-  </React.StrictMode>
+	<ApolloProvider client={client}>
+    <React.StrictMode>
+     <App />
+    </React.StrictMode>
+	</ApolloProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
