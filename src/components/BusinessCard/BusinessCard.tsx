2063bf7c62f90e6b7564d1f660cac5d235c1ebaa
manu-yaff <manuelyg2097@gmail.com>
2022-06-09 04:07:48 -0500

    feature: add search feature in desktop

--- src/components/BusinessCard/BusinessCard.tsx
+++ src/components/BusinessCard/BusinessCard.tsx
@@ -1,6 +1,6 @@
 import styles from './BusinessCard.module.scss';
-import { BusinessProps } from '../../types/Business';
 import { Business } from '../../types/Business';
+import { FaEye } from 'react-icons/fa';
 
 const BusinessCard = (props: Business) => {
 	return (
@@ -8,7 +8,7 @@ const BusinessCard = (props: Business) => {
 			<img src={props.photos} alt="" />
 			<div className={styles['flex-container']}>
 				<h2>{props.name}</h2>
-				<p>icon</p>
+				<p><FaEye /></p>
 			</div>
 			<p className={styles['card__location']}>
 				{props.location.address1}, {props.location.city}, {props.location.country}
