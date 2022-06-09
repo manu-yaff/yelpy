46c543dbdc8319bf571f0830f7a23ff89575879c
manu-yaff <manuelyg2097@gmail.com>
2022-06-08 22:03:39 -0500

    component: add button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,11 @@
+import styles from './Button.module.scss';
+
+const Button = () => {
+	return (
+		<>
+			<button type="button" className={styles['button']}>Search</button>
+		</>
+	)
+}
+
+export default Button;
--- src/pages/HomeScreen/HomeScreen.tsx
+++ src/pages/HomeScreen/HomeScreen.tsx
@@ -1,12 +1,17 @@
 import styles from './HomeScreen.module.scss';
 import Input from '../../components/Input/Input';
+import Button from '../../components/Button/Button';
+
 
 const HomeScreen = () => {
 	return (
 		<>
-			<div className={styles['inputs-container']}>
-				<Input placeholder="Search" roundedRight={false} />
-				<Input placeholder="Location" roundedRight={true} />
+			<div className={styles['main-container']}>
+				<div className={styles['inputs-container']}>
+					<Input placeholder="Search" roundedRight={false} />
+					<Input placeholder="Location" roundedRight={true} />
+				</div>
+				<Button />
 			</div>
 		</>
 	)
