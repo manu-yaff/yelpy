d557212bc332839a083dacee5cae79affb52811c
manu-yaff <manuelyg2097@gmail.com>
2022-08-27 16:08:19 -0500

    Add toast test

--- src/components/Toast/Toast.tsx
+++ src/components/Toast/Toast.tsx
@@ -1,16 +1,19 @@
-import { FunctionComponent } from "react";
-import style from "./Toast.module.scss";
+import { FunctionComponent } from 'react';
+import style from './Toast.module.scss';
 
 interface IProps {
-  children?: React.ReactNode;
-  toastType: "success" | "error";
+	children?: React.ReactNode;
+	toastType: 'success' | 'error';
 }
 
 const Toast: FunctionComponent<IProps> = ({ children, toastType }) => {
-  return (
-    <div className={`${style["toast"]} ${style[`toast--${toastType}`]}`}>
-      {children}
-    </div>
-  );
+	return (
+		<div
+			data-testid="toast"
+			className={`${style['toast']} ${style[`toast--${toastType}`]}`}
+		>
+			{children}
+		</div>
+	);
 };
 export default Toast;
