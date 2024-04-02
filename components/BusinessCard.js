713625b4cfef93480c157b3f61b8fda0e6eccc2a
manu-yaff <manuelyg2097@gmail.com>
2024-04-02 12:08:23 -0600

    refactor: BusinessCard component (add event listeners)

--- components/BusinessCard.js
+++ components/BusinessCard.js
@@ -1,41 +1,48 @@
-import { IMAGE_NOT_FOUND_PATH, ROUTES } from '../constants.js';
+import { IMAGE_NOT_FOUND_PATH } from '../constants.js';
 
 export function BusinessCard({ id, imageUrl, name, address, phone, reviews }) {
-  const cardId = `business-card-${id}`;
+  const componentContainer = document.createElement('div');
   const markup = `
-    <div id="${cardId}">
-      <img src="${imageUrl}" alt="${name} business image" />
-      <p>${name}</p>
-      <div>
-        <p>${address}</p>
+    <div>
+      <a href="/business/${id}">
+        <img src="${imageUrl}" alt="${name} business image" />
+        <p>${name}</p>
         <div>
-          <p>${reviews} reviews</p>
-          <p>${phone}</p>
+          <p>${address}</p>
+          <div>
+            <p>${reviews} reviews</p>
+            <p>${phone}</p>
+          </div>
         </div>
-      </div>
+      </a>
     </div>
   `;
+
+  initComponent();
+
+  function addEventListeners() {
+    const businessImg = componentContainer.querySelector('img');
+    businessImg.addEventListener('error', function handleImageLoadError() {
+      businessImg.src = IMAGE_NOT_FOUND_PATH;
+    });
+
+    const aTag = componentContainer.querySelector('a');
+    aTag.addEventListener('click', window.router.preventATagFromDefault);
+  }
+
+  function initComponent() {
+    componentContainer.insertAdjacentHTML('beforeend', markup);
+
+    addEventListeners();
+  }
 
   function getMarkup() {
     return markup;
   }
 
-  function addEventListenersToCard() {
-    const card = document.getElementById(cardId);
-    const imageCard = document.querySelector(`#${cardId} > img`);
-    card.addEventListener('click', function navigateToDetailView() {
-      router.navigateTo(`${ROUTES.detail}${id}`);
-    });
-
-    imageCard.addEventListener('error', function setDefaultImage() {
-      imageCard.src = IMAGE_NOT_FOUND_PATH;
-    });
+  function getContainer() {
+    return componentContainer;
   }
 
-  function render(container) {
-    container.insertAdjacentHTML('beforeend', markup);
-    addEventListenersToCard();
-  }
-
-  return { render, getMarkup };
+  return { getMarkup, getContainer };
 }
