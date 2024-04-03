c92470ef80906b8f1bc020929d155b34a664e6b0
manu-yaff <manuelyg2097@gmail.com>
2024-04-03 11:36:01 -0600

    refactor: Input component

--- components/Input.js
+++ components/Input.js
@@ -1,22 +1,36 @@
 // TODO: define where to validate inputs, here or in parent component
-export function Input({ type, labelText, placeholder, name }) {
+export function Input({ type, labelText, placeholder, name, required }) {
+  const componentContainer = document.createElement('div');
   const markup = `
-    <div>
-      <label for="${name}">${labelText}</label>
-      <input type="${type}" placeholder="${placeholder}" name="${name}" />
-    </div>
+    <label for="${name}">${labelText}</label>
+    <input
+      type="${type}"
+      placeholder="${placeholder}"
+      name="${name}"
+      ${required ? 'required' : ''}/>
   `;
+
+  initComponent();
+
+  function initComponent() {
+    componentContainer.insertAdjacentHTML('beforeend', markup);
+    const input = componentContainer.querySelector('input');
+
+    input.addEventListener('input', function setInputValue(event) {
+      input.value = event.target.value;
+    });
+  }
 
   function getMarkup() {
     return markup;
   }
 
-  function render(container) {
-    container.insertAdjacentHTML('beforeend', markup);
+  function getContainer() {
+    return componentContainer;
   }
 
   return {
     getMarkup,
-    render,
+    getContainer,
   };
 }
