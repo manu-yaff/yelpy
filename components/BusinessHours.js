ebefe59cc8efafac3d621456b8d9e4565895f3b7
manu-yaff <manuelyg2097@gmail.com>
2024-04-04 13:23:32 -0600

    refactor: BusinessHours component

--- components/BusinessHours.js
+++ components/BusinessHours.js
@@ -1,25 +1,42 @@
-import { OpeningDayHours, formatDayHours } from './DayHour.js';
+import { OpeningDayHours, formatDayHours } from './OpeningDayHours.js';
 
 export function BusinessHours({ isOpen, dayHours }) {
-  // TODO: extract function to render list in utils or similar
+  const componentContainer = document.createElement('div');
+  const dayHoursContainerId = 'day-hours-container';
   const markup = `
     <div>
-      <p>${isOpen ? 'Open' : 'Closed'}</p>
-      <div>
-        ${dayHours
-          .map((dayHour) => {
-            return OpeningDayHours({ weekday: dayHour[0], hours: dayHour[1] }).getMarkup();
-          })
-          .join('')}
-      </div>
+      <p>Business hours</p>
+      <p>${isOpen}</p>
+      <div id="${dayHoursContainerId}"></div>
     </div>
   `;
 
-  function getMarkup() {
-    return markup;
+  initComponent();
+
+  function initComponent() {
+    componentContainer.insertAdjacentHTML('beforeend', markup);
+
+    const dayHoursContainer = componentContainer.querySelector(`#${dayHoursContainerId}`);
+
+    if (dayHours.length == 0) {
+      dayHoursContainer.insertAdjacentHTML('beforeend', '<p>Day hours not available</p>');
+
+      return;
+    }
+
+    dayHours.forEach((item) => {
+      const [, ...rest] = item;
+
+      const dayHourComp = OpeningDayHours({ weekday: item[0], hours: rest.join(' - ') });
+      dayHoursContainer.append(dayHourComp.getContainer());
+    });
+  }
+
+  function getContainer() {
+    return componentContainer;
   }
 
   return {
-    getMarkup,
+    getContainer,
   };
 }
