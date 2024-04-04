23836edba423c704f9a7aff6f91392cd208904fb
manu-yaff <manuelyg2097@gmail.com>
2024-04-04 12:29:08 -0600

    refactor: Review component

--- components/ReviewList.js
+++ components/ReviewList.js
@@ -1,18 +1,39 @@
 import { Review } from './Review.js';
 
-export function ReviewList(reviews) {
-  function getMarkup() {
-    if (reviews.length == 0) return '<p>No reviews were found</p>';
+export function ReviewList({ items }) {
+  const componentContainer = document.createElement('div');
 
-    return `
-      <div>
-        <p>Reviews</p>
-        ${reviews.map((review) => Review(review).getMarkup()).join('')}
-      </div>
-    `;
+  const markup = `
+    <div>
+      <p>Reviews</p>
+      <div id="reviews-container"></div>
+    </div>
+  `;
+
+  initComponent();
+
+  function initComponent() {
+    if (items.length == 0) {
+      componentContainer.insertAdjacentHTML('beforeend', '<p>No reviews were found</p>');
+      return;
+    }
+
+    componentContainer.insertAdjacentHTML('beforeend', markup);
+
+    const reviewsContainer = componentContainer.querySelector('#reviews-container');
+
+    items.forEach((item) => {
+      const reviewComp = Review(item);
+
+      reviewsContainer.appendChild(reviewComp.getContainer());
+    });
+  }
+
+  function getContainer() {
+    return componentContainer;
   }
 
   return {
-    getMarkup,
+    getContainer,
   };
 }
