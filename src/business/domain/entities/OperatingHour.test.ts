002171e30c0e73573be0a5afe19cb379f49a27b5
manu-yaff <manuelyg2097@gmail.com>
2025-03-11 12:18:29 -0600

    feat: add OperatingHour entity

---
+++ src/business/domain/entities/OperatingHour.test.ts
@@ -0,0 +1,27 @@
+import { describe, expect, it } from 'vitest'
+import { OperatingHour, OperatingHourType } from './OperatingHour'
+
+describe(OperatingHour.name, () => {
+  describe(OperatingHour.prototype.formatted.name, () => {
+    it('should format hour object', () => {
+      // Arrange
+      const mockHour: OperatingHourType = {
+        start: '0800',
+        end: '1700',
+        day: 1,
+      }
+
+      // Act
+      const hour = new OperatingHour(mockHour)
+
+      // Assert
+      expect(hour.formatted()).toEqual(
+        expect.objectContaining({
+          start: '08:00',
+          end: '17:00',
+          day: 'Monday',
+        })
+      )
+    })
+  })
+})
---
+++ src/business/domain/entities/OperatingHour.ts
@@ -0,0 +1,23 @@
+export type OperatingHourType = {
+  start: string
+  end: string
+  day: number
+}
+
+const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+export class OperatingHour {
+  constructor(private data: OperatingHourType) {}
+
+  public formatted() {
+    return {
+      day: weekdays[this.data.day],
+      start: this.formatHour(this.data.start),
+      end: this.formatHour(this.data.end),
+    }
+  }
+
+  private formatHour(hour: string) {
+    return `${hour.slice(0, 2)}:${hour.slice(2, 4)}`
+  }
+}
