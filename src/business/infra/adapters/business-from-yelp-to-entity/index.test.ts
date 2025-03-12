cea28336b7d8a3a66b8c538e0c03f2320104f680
manu-yaff <manuelyg2097@gmail.com>
2025-03-11 18:03:04 -0600

    refactor: business adapter

---
+++ src/business/infra/adapters/business-from-yelp-to-entity/index.test.ts
@@ -0,0 +1,28 @@
+import { describe, expect, it } from 'vitest'
+import { BusinessMapper } from '.'
+import { Business } from '../../../domain/entities/Business'
+import { BusinessFromYelp } from '../../repositories/yelp-business-repository.types'
+
+describe(BusinessMapper.name, () => {
+  describe(BusinessMapper.fromYelp.name, () => {
+    it('should adapt business from yelp api to business entity', () => {
+      // Arrange
+      const mockBusinessFromYelp: BusinessFromYelp = {
+        id: 'TT7QK8t7A-0vvGUr1q-lbg',
+        location: {
+          formatted_address: 'Plaza Tapatía\nPaseo Degollado #63',
+        },
+        name: 'Los Potrillos',
+        display_phone: '+52 33 3613 6136',
+        review_count: 7,
+        photos: ['https://s3-media4.fl.yelpcdn.com/bphoto/cLxG4S5xsvrdLCdnjRAOGg/o.jpg'],
+      }
+
+      // Act
+      const business = BusinessMapper.fromYelp(mockBusinessFromYelp)
+
+      // Assert
+      expect(business).toBeInstanceOf(Business)
+    })
+  })
+})
---
+++ src/business/infra/adapters/business-from-yelp-to-entity/index.ts
@@ -0,0 +1,15 @@
+import { Business } from '../../../domain/entities/Business'
+import { BusinessFromYelp } from '../../repositories/yelp-business-repository.types'
+
+export class BusinessMapper {
+  static fromYelp(business: BusinessFromYelp): Business {
+    return new Business({
+      id: business.id,
+      name: business.name,
+      phone: business.display_phone,
+      address: business.location.formatted_address,
+      reviewCount: business.review_count,
+      photos: business.photos,
+    })
+  }
+}
