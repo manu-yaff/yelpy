a48a39c94a8379fc4eeba178bed7bc361bcd3d21
manu-yaff <manuelyg2097@gmail.com>
2025-03-12 17:22:16 -0600

    chore: update tests description and comments

--- src/business/infra/presenters/components/BusinessReviews/index.test.tsx
+++ src/business/infra/presenters/components/BusinessReviews/index.test.tsx
@@ -1,27 +1,30 @@
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
-import { describe, expect, test } from 'vitest'
+import { describe, expect, it } from 'vitest'
 import BusinessReviews from '.'
 import { Review } from '../../../../domain/entities/Review'
 import { User } from '../../../../domain/entities/User'
 
-describe('BusinessReviews Component', () => {
-  const mockReviews = [
-    new Review({
-      id: '1',
-      timeCreated: '2024-03-12',
-      rating: 5,
-      text: 'Test Review',
-      user: new User({
-        name: 'John Doe',
-        profileUrl: 'https://example.com/johndoe',
+describe(BusinessReviews.name, () => {
+  it('renders reviews correctly', () => {
+    // Arrange
+    const mockReviews = [
+      new Review({
+        id: '1',
+        timeCreated: '2024-03-12',
+        rating: 5,
+        text: 'Test Review',
+        user: new User({
+          name: 'John Doe',
+          profileUrl: 'https://example.com/johndoe',
+        }),
       }),
-    }),
-  ]
+    ]
 
-  test('renders reviews correctly', () => {
+    // Act
     render(<BusinessReviews reviews={mockReviews} />)
 
+    // Assert
     expect(screen.getByText('2024-03-12')).toBeInTheDocument()
     expect(screen.getByText('5')).toBeInTheDocument()
     expect(screen.getByText('Test Review')).toBeInTheDocument()
