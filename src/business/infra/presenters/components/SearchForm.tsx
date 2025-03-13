619e2816fcdd164ab6ccd797e627b1e1401ae77c
manu-yaff <manuelyg2097@gmail.com>
2025-03-13 12:43:12 -0600

    feat: auto load business search inputs

--- src/business/infra/presenters/components/SearchForm.tsx
+++ src/business/infra/presenters/components/SearchForm.tsx
@@ -1,9 +1,11 @@
 import { ReactNode, useState } from 'react'
-import { Outlet, useNavigate } from 'react-router'
+import { Outlet, useNavigate, useSearchParams } from 'react-router'
 
 function SearchForm(): ReactNode {
-  const [term, setTerm] = useState('')
-  const [location, setLocation] = useState('')
+  const [searchParams] = useSearchParams()
+
+  const [term, setTerm] = useState(searchParams.get('term') || '')
+  const [location, setLocation] = useState(searchParams.get('location') || '')
   const navigate = useNavigate()
 
   const handleSubmit = (e: React.FormEvent): void => {
@@ -17,21 +19,23 @@ function SearchForm(): ReactNode {
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <input
-          placeholder="tacos"
-          value={term}
-          required
-          onChange={(e) => setTerm(e.target.value)}
-        />
-        <input
-          placeholder="san francisco"
-          value={location}
-          required
-          onChange={(e) => setLocation(e.target.value)}
-        />
-        <button type="submit">Search</button>
-      </form>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <input
+            placeholder="tacos"
+            value={term}
+            required
+            onChange={(e) => setTerm(e.target.value)}
+          />
+          <input
+            placeholder="san francisco"
+            value={location}
+            required
+            onChange={(e) => setLocation(e.target.value)}
+          />
+          <button type="submit">Search</button>
+        </form>
+      </div>
       <Outlet />
     </>
   )
