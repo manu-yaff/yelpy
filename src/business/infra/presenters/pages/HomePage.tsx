a2baf1f57bed1ce67558262f67ffbe1197fed0ba
manu-yaff <manuelyg2097@gmail.com>
2025-03-13 12:42:51 -0600

    feat: add basic home page

--- src/business/infra/presenters/pages/HomePage.tsx
+++
@@ -1,7 +0,0 @@
-import { ReactNode } from 'react'
-
-function HomePage(): ReactNode {
-  return <h1>Welcome, pls try searching for something</h1>
-}
-
-export default HomePage
---
+++ src/business/infra/presenters/pages/HomePage/index.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from 'react'
+
+function HomePage(): ReactNode {
+  return (
+    <>
+      <p>Welcome!</p>
+      <p>
+        This app uses Yelp Fusion API, which allows you to ge the best local content and reviews
+        from the millions of business around the world
+      </p>
+    </>
+  )
+}
+
+export default HomePage
